09a43a8e7bd6bd817c1d047a5a1bd4282b77a6a0
DKCisco <dillan.k.craig@gmail.com>
2023-03-27 22:42:10 +0000

    Testing out maths

---
+++ arith.py
@@ -0,0 +1,39 @@
+# This equals 8
+add_1 = 3 + 5
+add_2 = 5 + 3
+
+# This equals 14
+sub_1 = 20 - 6
+
+# This equals negative 14
+sub_2 = 6 - 20
+
+
+print(sub_2)
+
+# This equals 35
+mult_1 = 7 * 5
+mult_2 = 5 * 7
+
+# This equals 5
+div_1 = 100 / 20
+
+# This equals 5
+div_2 = 20 / 100
+print(div_1)
+print(type(div_1))
+
+# This equals 125
+exp_1 = 5 ** 3
+
+# This equals 3
+
+floor = 83 // 25
+print(floor)
+
+# This will return 2
+mod_1 = 8 % 3
+print(mod_1)
+
+result = 3 + 7 * 15 / 22
+print(result)
