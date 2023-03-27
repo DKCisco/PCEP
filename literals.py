889d31ef36931c87a78fff58d8841ca8d3dc833b
DKCisco <dillan.k.craig@gmail.com>
2023-03-27 20:11:38 +0000

    Learning about literals

---
+++ literals.py
@@ -0,0 +1,15 @@
+x = 10.0
+y = 17.5
+z = 12.256
+
+#Boolean Literals
+true_literal = True
+false_liters = False
+
+
+#String literal
+username = "BenFinkel"
+
+print(username)
+print('user "name"')
+print("user 'name'")
