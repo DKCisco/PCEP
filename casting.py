1e48874781c400d23f748a9ab85b9f1349109afe
DKCisco <dillan.k.craig@gmail.com>
2023-03-27 19:51:50 +0000

    Learning about casting

---
+++ casting.py
@@ -0,0 +1,22 @@
+x = 10
+
+print(type(x))
+
+print(float(x))
+
+print(type(float(x)))
+
+y = float(x)
+
+z = str(x)
+
+print(type(z))
+
+print(z)
+
+answer = input("Type a numebr: ")
+
+answer_int = int(answer)
+
+print(answer * 7)
+print(answer_int * 7)
