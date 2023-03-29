2a495a3b0941282cd18b462b12e20e30c261c20d
DKCisco <dillan.k.craig@gmail.com>
2023-03-29 21:00:42 +0000

    String operators

---
+++ string_operators.py
@@ -0,0 +1,17 @@
+name_1 = "Ben"
+name_2 = "Finkel"
+
+last_name = name_1 + " " + name_2
+
+print(last_name)
+
+text_value = "Reapeat Me "
+
+text_value_50 = text_value * 50
+
+text_value_3 = text_value[3]
+text_value_3_5 = text_value[1:3]
+
+print(text_value_50)
+print(text_value_3)
+print(text_value_3_5)
