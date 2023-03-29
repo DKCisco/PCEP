551b0965bccda6903984be1af58194ad4fab510a
DKCisco <dillan.k.craig@gmail.com>
2023-03-29 20:46:22 +0000

    Relational boolean

---
+++ relational.py
@@ -0,0 +1,22 @@
+x = 10
+y = 15
+z = 10.0
+
+print(x > y)
+
+print(y > x)
+
+print(x == y)
+
+print(x != y)
+
+if (x > y):
+    print("X is bigger than Y!")
+else:
+    print("Y is bigger than (or equal to) X!")
+    
+print(z == x)
+
+print(z == x or x == y)
+
+print(z == x and x == y)
