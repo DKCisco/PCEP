cff31e2dcb3af12e41b08779b7a259526eb4e589
DKCisco <dillan.k.craig@gmail.com>
2023-04-03 18:47:04 +0000

    Print funciton testing

---
+++ print.py
@@ -0,0 +1,12 @@
+print("Hello")
+print("Dude")
+
+myName = "Ben"
+print(myName)
+
+print("Hello", myName, "how","are","you?")
+
+print("Hello" + myName)
+
+age = 42
+print("Hello", "Ben", "You are:", age, "years old.")
