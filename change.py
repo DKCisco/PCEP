da6aaeb32639b6a5a5f30251a6ae7e8cb668431f
DKCisco <dillan.k.craig@gmail.com>
2023-04-03 18:24:14 +0000

    Change making program

---
+++ change.py
@@ -0,0 +1,18 @@
+changeAmt = int(input("Enter the change due: "))
+
+numQtrs = int(changeAmt // 25) #Figure out the number of quarters
+changeAmt = int(changeAmt - (numQtrs * 25))
+
+numDimes = int(changeAmt // 10) #Figure out the number of dimes
+changeAmt -= (numDimes * 10)
+
+numNickels = int(changeAmt //5) #Figure out number of nickels
+changeAmt -= (numNickels * 5)
+
+numPennies = int(changeAmt // 1) #Figure out number of pennies
+changeAmt -= (numPennies * 1)
+
+print("Quarters: " + str(numQtrs))
+print("Dimes: " + str(numDimes))
+print("Nickels: " + str(numNickels))
+print("Pennies: " + str(numPennies))
