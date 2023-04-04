a1add270b829b78cb9cff2ef73d444ae643ac08e
DKCisco <dillan.k.craig@gmail.com>
2023-04-04 18:56:33 +0000

    Using while loops

---
+++ while_loop.py
@@ -0,0 +1,10 @@
+x = 10
+y = 15
+
+while x < y :
+    print("This is the current vlaue of x")
+    print(str(x))
+    print("Now we're going to change x!")
+    x += 1
+
+print("Outside the loop this will always run.")
