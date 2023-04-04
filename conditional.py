07ea4861d2b130bdf634d3a08156f13169483d9c
DKCisco <dillan.k.craig@gmail.com>
2023-04-04 21:22:02 +0000

    Updating multiple

---
+++ conditional.py
@@ -0,0 +1,18 @@
+
+x = 20
+y = 15
+is_this_true = False
+
+if is_this_true:
+    print("X is in fact, greater than Y.")
+    print("I hope you already knew that.")
+    print("Goodbye")
+elif x > y:
+    print("We triggered the second clause.")
+    
+
+else:
+    print("Y is in fact, greater than X.")
+    print("But you knew that.")
+
+print("This always runs.")
