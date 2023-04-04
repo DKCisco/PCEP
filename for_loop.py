a9f05e9e028ea1776a67d8e78feca34c601d3962
DKCisco <dillan.k.craig@gmail.com>
2023-04-04 19:58:28 +0000

    Testing a for loop

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+for x in range(1500, 1, -10):
+    print("This is the current value of x")
+    print(str(x))
+    print("Now we're going to change x!")
+
+print("Outside the loop this will always run.")
+
+names = ["Ben", "Diane", "Pat", "Sam"]
+
+for name in names:
+    print(f"The user's name is {name}")
