144785dc56f200377ed0b09fcaef0c63678efe94
DKCisco <dillan.k.craig@gmail.com>
2023-04-06 18:51:34 +0000

    Playing w/ strings

---
+++ string_loop.py
@@ -0,0 +1,15 @@
+
+
+my_name = "Dillan"
+print(id(my_name))
+
+#for letter in my_name:
+    #print(letter)
+    #print(my_name[2])
+
+#my_name[2] = "q"
+
+my_name = "Diqlan"
+print(id(my_name))
+name_2 = "Diqlan"
+print(id(name_2))
