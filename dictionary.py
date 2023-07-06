3308f0a37fb14ed30cd8ff552b2fa40c9eeda3a2
DKCisco <dillan.k.craig@gmail.com>
2023-07-06 20:54:09 +0000

    dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,8 @@
 studentGrades = {"Ben": 92, "Diane": 97, "Sam": 81}
 
 print(studentGrades["Diane"])
+
+myCars = ({"Make": "Ford", "Model": "Model A", "Year": 2031, "colors":("blue", "grey")},
+        {"Make": "Chevy", "Model": "Cavalier", "Year": 1997})
+
+print(myCars[0]["colors"][1])
